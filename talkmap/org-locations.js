ca0e163f0a9cfceb13be2266fe92a8b25c6e9193
Nick-Bertoldo <nicolas.bertoldo94@gmail.com>
2025-03-27 17:43:13 +0100

    Add map functionality and update talk details for green waste management

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,7 +5,12 @@ var addressPoints = [
     13.3989367
   ],
   [
-    "The potential environmental benefit of the ecodesign approach: a case study in the footwear industry<br />XVI conference of the Associazione Rete Italiana LCA; Milan, Italy",
+    "Life Cycle Assessment of Green Waste Management: Optimizing Composting Facility Distribution in the Milan Metropolitan Area<br />XIX conference of the Associazione Rete Italiana LCA; Cortina d'Ampezzo",
+    46.5383332,
+    12.1373506
+  ],
+  [
+    "The potential environmental benefit of the ecodesign approach: a case study in the footwear industry<br />XVII conference of the Associazione Rete Italiana LCA; Milan, Italy",
     45.4641943,
     9.1896346
   ]
